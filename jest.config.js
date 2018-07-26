7e50f14a09cb9a79af6fd7d1989eed614db0cce2
Daniel Marczydło <daniel.marczydlo@gmail.com>
2018-07-26 20:13:10 +0200

    jest update

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testPathIgnorePatterns: [
+        './.idea',
+        './.next',
+        './lib',
+        './node_modules'
+    ],
+    collectCoverage: true,
+    coveragePathIgnorePatterns: [
+        "./node_modules",
+    ]
+};
